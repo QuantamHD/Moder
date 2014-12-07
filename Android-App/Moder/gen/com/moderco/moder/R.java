af3004dde0bb26b1553db6c162cdcedde572cb77
Flaque <econrad@zagmail.gonzaga.edu>
2014-12-07 14:35:17 -0800

    Basic Login Screen
    The username and password is “admin” and “admin”

--- Android-App/Moder/gen/com/moderco/moder/R.java
+++ Android-App/Moder/gen/com/moderco/moder/R.java
@@ -1654,13 +1654,15 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f050030;
         public static final int action_mode_bar_stub=0x7f05002f;
         public static final int action_mode_close_button=0x7f050022;
-        public static final int action_settings=0x7f05004d;
+        public static final int action_settings=0x7f050052;
         public static final int activity_chooser_view_content=0x7f050023;
         public static final int always=0x7f05000d;
-        public static final int bar=0x7f050041;
+        public static final int bar=0x7f050046;
         public static final int beginning=0x7f050012;
         public static final int bottom=0x7f050016;
-        public static final int camera=0x7f050045;
+        public static final int button1=0x7f050045;
+        public static final int button2=0x7f050044;
+        public static final int camera=0x7f05004a;
         public static final int checkbox=0x7f05002b;
         public static final int collapseActionView=0x7f05000f;
         public static final int decor_content_parent=0x7f050031;
@@ -1672,27 +1674,28 @@ containing a value of this type.
         public static final int end=0x7f050014;
         public static final int expand_activities_button=0x7f050024;
         public static final int expanded_menu=0x7f05002a;
-        public static final int gears=0x7f050044;
+        public static final int gears=0x7f050049;
         public static final int home=0x7f050017;
         public static final int homeAsUp=0x7f050007;
         public static final int icon=0x7f050028;
         public static final int ifRoom=0x7f05000c;
         public static final int image=0x7f050025;
-        public static final int linearLayout1=0x7f050043;
+        public static final int linearLayout1=0x7f050048;
         public static final int listMode=0x7f050002;
         public static final int list_item=0x7f050027;
-        public static final int listview_background_shape=0x7f05004c;
+        public static final int listview_background_shape=0x7f050051;
         public static final int middle=0x7f050013;
         public static final int never=0x7f05000b;
-        public static final int noButton=0x7f05004a;
+        public static final int noButton=0x7f05004f;
         public static final int none=0x7f050004;
         public static final int normal=0x7f050001;
-        public static final int photoProfile=0x7f050049;
+        public static final int password=0x7f050041;
+        public static final int photoProfile=0x7f05004e;
         public static final int progress_circular=0x7f05001e;
         public static final int progress_horizontal=0x7f05001f;
         public static final int radio=0x7f05002d;
-        public static final int rect=0x7f050047;
-        public static final int search=0x7f050046;
+        public static final int rect=0x7f05004c;
+        public static final int search=0x7f05004b;
         public static final int search_badge=0x7f050037;
         public static final int search_bar=0x7f050036;
         public static final int search_button=0x7f050038;
@@ -1703,22 +1706,24 @@ containing a value of this type.
         public static final int search_plate=0x7f05003b;
         public static final int search_src_text=0x7f05003c;
         public static final int search_voice_btn=0x7f050040;
-        public static final int shadow=0x7f050042;
+        public static final int shadow=0x7f050047;
         public static final int shortcut=0x7f05002c;
         public static final int showCustom=0x7f050009;
         public static final int showHome=0x7f050006;
         public static final int showTitle=0x7f050008;
         public static final int split_action_bar=0x7f05001b;
+        public static final int submitButton=0x7f050043;
         public static final int submit_area=0x7f05003e;
         public static final int tabMode=0x7f050003;
-        public static final int textView1=0x7f050048;
+        public static final int textView1=0x7f05004d;
         public static final int title=0x7f050029;
         public static final int top=0x7f050015;
         public static final int up=0x7f050018;
         public static final int useLogo=0x7f050005;
+        public static final int username=0x7f050042;
         public static final int withText=0x7f05000e;
         public static final int wrap_content=0x7f050000;
-        public static final int yesButton=0x7f05004b;
+        public static final int yesButton=0x7f050050;
     }
     public static final class integer {
         /**  The maximum number of action buttons that should be permitted within
@@ -1769,8 +1774,9 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030014;
         public static final int abc_search_view=0x7f030015;
         public static final int abc_simple_dropdown_hint=0x7f030016;
-        public static final int activity_main=0x7f030017;
-        public static final int support_simple_spinner_dropdown_item=0x7f030018;
+        public static final int activity_login=0x7f030017;
+        public static final int activity_main=0x7f030018;
+        public static final int support_simple_spinner_dropdown_item=0x7f030019;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
